87702e545dc73225a460c58c51a68f9d81d4518c
marlonCorreia <malilocsimas26@gmail.com>
2021-05-15 00:04:32 -0300

    initial commit =]

---
+++ article/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from author.models import Author
+
+# Create your models here.
+class Article(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    author = models.ManyToManyField(Author)
+    title = models.CharField(max_length=100)
+    category = models.TextField(max_length=100)
+    summary = models.TextField(max_length=200)
+    first_paragraph = models.TextField()
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
---
+++ author/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    picture = models.URLField()
+
+    def __str__(self):
+        return self.name
