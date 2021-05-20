22d2117d84093963c245871017db283092fc50a4
marlonCorreia <malilocsimas26@gmail.com>
2021-05-20 20:35:53 -0300

    added authorView url

--- author/urls.py
+++ author/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('authors', views.AuthorView.as_view(), name='author_view'),
+    path('authors/', views.AuthorList.as_view(), name='author_list'),
+    path('authors/<int:pk>', views.AuthorView.as_view(), name='author_view')
 ]
